9df069552f8436beac581e8946ae219f8b73e92d
Ashkan <ashkan.m10@gmail.com>
2018-04-06 22:41:44 +0430

    UML finalized

---
+++ Clash/src/com/company/Models/Defences/Defence.java
@@ -0,0 +1,14 @@
+package com.company.Models.Defences;
+
+public abstract class Defence {
+    public int jsonType;
+    public int cost;
+    public int buildDuration;
+    public int strength;
+    public int damage;
+    public int radius;
+    public int pointsGainedWhenDistructed;
+    public int resourceGainedWhenDistructed;
+    public int level;
+
+}
