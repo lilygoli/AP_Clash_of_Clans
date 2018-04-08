f64f81020a7185dca3b3a219cf9192e06e3968c5
unknown <arad.mohammadi99@gmail.com>
2018-04-08 19:22:32 +0430

    uml

---
+++ Clash/src/com/company/Models/Soldiers/Soldier.java
@@ -0,0 +1,24 @@
+package com.company.Models.Soldiers;
+
+import com.company.Enums.Directon;
+
+import java.util.ArrayList;
+
+public abstract class Soldier {
+
+    static ArrayList<Soldier> soldierSubClasses;
+    private int cost;
+    private int buildDuration;
+    private int health;
+    private int damage;
+    private int radius;
+    private int maxSpeed;
+    private int level;
+    private int addedHealth;
+    private int addedDamage;
+    private int unlockLevel;
+    private double x , y;
+    boolean canFly;
+    private Directon directon;
+    
+}
