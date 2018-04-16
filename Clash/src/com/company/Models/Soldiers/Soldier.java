c5bf5e1f3220817c4c5111beaa570727ff603304
unknown <arad.mohammadi99@gmail.com>
2018-04-16 11:11:13 +0430

    make functions compelete(without findDest)

--- Clash/src/com/company/Models/Soldiers/Soldier.java
+++ Clash/src/com/company/Models/Soldiers/Soldier.java
@@ -17,11 +17,12 @@ public abstract class Soldier {
     private int addedHealth;
     private int addedDamage;
     private int unlockLevel;
-    private double x , y;
+    private double x, y;
     private boolean canFly;
     private boolean isDead;
     private Direction directon;
 
+    private final double MOVE_PER_TURN = 1 / maxSpeed;
     //private distanceCalc()
 
     public static ArrayList<Soldier> getSoldierSubClasses() {
@@ -88,56 +89,57 @@ public abstract class Soldier {
         return directon;
     }
 
-    public void upgrade(){
-        ;
+    public void upgrade() {
+        damage += addedDamage;
+        health += addedHealth;
     }
 
-    public Direction findDirection(Village enemyVillage , Cell destination){
+    public Direction findDirection(Village enemyVillage, Cell destination) {
         return Direction.UP;
     }
 
-    public boolean hasReachedDestination(Village enemyVillage){
-        if (Math.sqrt(Math.pow(x - findDestination(enemyVillage).getX() , 2) + Math.pow(y - findDestination(enemyVillage).getY() , 2)) <= radius){
+    public boolean hasReachedDestination(Village enemyVillage) {
+        if (Math.sqrt(Math.pow(x - findDestination(enemyVillage).getX(), 2) + Math.pow(y - findDestination(enemyVillage).getY(), 2)) <= radius) {
             return true;
         }
         return false;
     }
 
-    public Cell findDestination(Village enemyVillage){
+    public Cell findDestination(Village enemyVillage) {
         Cell destination = new Cell();
         double minDistance = 100;
         for (int i = 0; i < 30; i++) {
             for (int j = 0; j < 30; j++) {
-                if (Math.sqrt(Math.pow(x - i, 2) + Math.pow(y - j , 2)) < minDistance){
+                if (Math.sqrt(Math.pow(x - i, 2) + Math.pow(y - j, 2)) < minDistance) {
                     destination = enemyVillage.getMap()[i][j];
-                    minDistance = Math.sqrt(Math.pow(x - i, 2) + Math.pow(y - j , 2));
+                    minDistance = Math.sqrt(Math.pow(x - i, 2) + Math.pow(y - j, 2));
                 }
             }
         }
         return destination;
     }
 
-    public void moveSoldier(Direction directon , Village enemyVillage){
-        directon = findDirection(enemyVillage , findDestination(enemyVillage));
-        if (directon == Direction.LEFT){
-            x = x - 1/maxSpeed;
-        }
-        else if (directon == Direction.RIGHT){
-            x = x + 1/maxSpeed;
-        }
-        else if (directon == Direction.UP){
-            y = y + 1/maxSpeed;
-        }
-        else if (directon == Direction.DOWN){
-            y = y - 1/maxSpeed;
+    public void moveSoldier(Direction directon) {
+        if (directon == Direction.LEFT) {
+            x = x - MOVE_PER_TURN;
+        } else if (directon == Direction.RIGHT) {
+            x = x + MOVE_PER_TURN;
+        } else if (directon == Direction.UP) {
+            y = y + MOVE_PER_TURN;
+        } else if (directon == Direction.DOWN) {
+            y = y - MOVE_PER_TURN;
         }
     }
 
-    public void attackTarget(Cell target){
-
+    public void attackTarget(Cell target, Village enemyVillage) {
+        if (hasReachedDestination(enemyVillage)) {
+            target.setStrength(target.getStrength() - damage);
+            if (target.getStrength() <= 0) {
+                target.setRuined(true);
+            }
+        } else {
+            directon = findDirection(enemyVillage, findDestination(enemyVillage));
+            moveSoldier(directon);
+        }
     }
-
-
-
-    
 }
