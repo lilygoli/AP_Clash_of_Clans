71953fd4a5b274a0c0662f5be45a3937a7ec2e16
leily goli <leilygoli100@yahoo.com>
2018-04-18 01:10:40 +0430

    Camp class was implemented completely.some methods were added to show the list of soldiers in a camp.

--- Clash/src/com/company/Models/Buildings/Camp.java
+++ Clash/src/com/company/Models/Buildings/Camp.java
@@ -4,6 +4,7 @@ import com.company.Models.Soldier.Soldier;
 import com.company.View.View;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class Camp extends Building {
 
@@ -27,5 +28,50 @@ public class Camp extends Building {
     public void showInfoMenu(){
         String infoMenu = "1. Overall info\n2. Upgrade info\n3. Capacity info\n4. Back";
         View.show(infoMenu);
+    }
+    public void showCapacityInfo(int numberOfSoldiers,int numberOfCamps){
+        String capacityInfo="Your camps capacity is "+numberOfSoldiers+"/"+numberOfCamps*capacity+".";
+        View.show(capacityInfo);
+    }
+    public void showSoldiers(){
+        HashMap<Class,Integer> soldierClasses=matchEachSoldierTypeWithNumber();
+        StringBuilder soldierList= new StringBuilder();
+        for (Class soldierClass: soldierClasses.keySet()
+             ) {
+            soldierList.append(soldierClass.toString().split(" ")[1]).append(" x").append(soldierClasses.get(soldierClass)).append("\n");
+        }
+        soldierList = new StringBuilder(soldierList.toString().trim());
+        View.show(soldierList.toString());
+
+    }
+    private HashMap<Class,Integer> matchEachSoldierTypeWithNumber(){
+        HashMap<Class,Integer> soldierClasses=new HashMap<Class,Integer>();
+        for (Soldier soldier: soldiers
+                ) {
+            if(!soldierClasses.containsKey(soldier.getClass())){
+                soldierClasses.put(soldier.getClass(),findNumberOfCertainSoldierType(soldier.getClass()));
+            }
+        }
+        return soldierClasses;
+    }
+    private int findNumberOfCertainSoldierType(Class soldierClass){
+        int number=0;
+        for (Soldier soldier:soldiers
+             ) {
+            if(soldier.getClass()==soldierClass){
+                number++;
+            }
+        }
+        return number;
+    }
+    public void healCamp(){
+        for (Soldier soldier:soldiers
+             ) {
+            //soldier.heal implemented in soldier class
+        }
+    }
+
+    public ArrayList<Soldier> getSoldiers() {
+        return soldiers;
     }
 }
