4b70bc52557cb9ff5ea44aa8e77481cea8b52cc8
unknown <arad.mohammadi99@gmail.com>
2018-04-21 11:40:43 +0430

    add showAttack menu

--- Clash/src/com/company/Models/Game.java
+++ Clash/src/com/company/Models/Game.java
@@ -1,6 +1,7 @@
 package com.company.Models;
 
 import com.company.Models.Soldiers.Soldier;
+import com.company.View;
 
 import java.util.ArrayList;
 
@@ -17,10 +18,16 @@ public class Game {
         village.showSourcesInfo();
     }
 
-    public void showAttackMenu(){
+    public String showAttackMenu(){
+        StringBuilder finalString = new StringBuilder();
+        finalString.append("1.Load Map\n");
+        int cnt = 2;
         for (Game game : GameCenter.getGames()) {
-            ;
+            finalString.append(cnt + "." + game.playerName + "\n");
+            cnt++;
         }
+        finalString.append(cnt + ".Back\n");
+        return finalString.toString();
     }
     public void showBuildings() {
 
