7c54bf9e97437b76b70b4c1074376ec4ce397ef4
unknown <arad.mohammadi99@gmail.com>
2018-04-29 14:01:48 +0430

    add main

--- Clash/src/com/company/Main.java
+++ Clash/src/com/company/Main.java
@@ -1,8 +1,12 @@
 package com.company;
 
 
+import com.company.Models.Controller;
 
 public class Main {
     public static void main(String[] args) {
+        Controller controller = new Controller();
+        View view = new View();
+            controller.mainCommandAnalyzer();
     }
 }
