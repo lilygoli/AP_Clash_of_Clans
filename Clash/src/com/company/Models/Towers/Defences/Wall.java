23fcc66cb4a2c0b8ceec729e9edb6f2492544eed
leily goli <leilygoli100@yahoo.com>
2018-06-09 12:49:34 +0430

    imageView added to buildings

--- Clash/src/com/company/Models/Towers/Defences/Wall.java
+++ Clash/src/com/company/Models/Towers/Defences/Wall.java
@@ -2,6 +2,8 @@ package com.company.Models.Towers.Defences;
 
 import com.company.Models.Config;
 import com.company.Models.Towers.Cell;
+import javafx.scene.image.ImageView;
+
 
 public class Wall extends Cell{
     public Wall(int number,int level) {
