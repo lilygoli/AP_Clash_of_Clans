ba08b7c0172254654e2bb64cb5b3a971aba25a7a
unknown <arad.mohammadi99@gmail.com>
2018-06-23 17:24:14 +0430

    some pictures add

--- Clash/src/com/company/Models/Towers/Defences/WizardTower.java
+++ Clash/src/com/company/Models/Towers/Defences/WizardTower.java
@@ -40,7 +40,6 @@ public class WizardTower extends Defence {
                         if (target.getHealth() <= 0) {
                             i.remove();;
                             target.getImageView().setImage(null);
-
                         }
                     }
                 }
