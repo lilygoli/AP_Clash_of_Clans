978f5ed51b484a044931c18e9b4b37328e056053
Ashkan <ashkan.m10@gmail.com>
2018-07-10 18:35:10 +0430

    bug fix

--- Clash/src/com/company/Multiplayer/LiveAttackStreamer.java
+++ Clash/src/com/company/Multiplayer/LiveAttackStreamer.java
@@ -1,12 +1,15 @@
 package com.company.Multiplayer;
 
+import com.company.Models.Soldiers.Soldier;
 import com.company.UIs.AttackMapUI;
+import com.company.UIs.MapUI;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.ObjectOutput;
 import java.io.ObjectOutputStream;
 import java.net.DatagramPacket;
+import java.util.ArrayList;
 
 public class LiveAttackStreamer extends Thread{
     public void run() {
@@ -14,7 +17,18 @@ public class LiveAttackStreamer extends Thread{
             ByteArrayOutputStream bStream = new ByteArrayOutputStream();
             try {
                 ObjectOutput oo = new ObjectOutputStream(bStream);
-                oo.writeObject(new liveStreamingMessage());
+                liveStreamingMessage lsm = new liveStreamingMessage();
+                ArrayList<Soldier> troops;
+                ArrayList<Integer> healths = new ArrayList<>();
+                System.out.println(MapUI.getController().getGame().getTroops());
+                for (int i = 0; i <30 ; i++) {
+                    for (int j = 0; j <30 ; j++) {
+                        healths.add(MapUI.getController().getGame().getVillage().getMap()[i][j].getStrength());
+                    }
+                }
+                lsm.setTroops(MapUI.getController().getGame().getTroops());
+                lsm.setHealths(healths);
+                oo.writeObject(lsm);
                // System.out.println("streamer sent"+AttackMapUI.getController().getGame());
                 oo.close();
             } catch (Exception e) {
