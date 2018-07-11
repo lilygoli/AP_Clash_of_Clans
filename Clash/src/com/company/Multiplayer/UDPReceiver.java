99ba603358d7ca159b6daae2099470cb1f140f02
leily goli <leilygoli100@yahoo.com>
2018-07-11 12:53:07 +0430

    live streaming bugfix

--- Clash/src/com/company/Multiplayer/UDPReceiver.java
+++ Clash/src/com/company/Multiplayer/UDPReceiver.java
@@ -1,11 +1,17 @@
 package com.company.Multiplayer;
 
+import com.company.Models.Config;
 import com.company.Models.Game;
+import com.company.Models.Resource;
 import com.company.Models.Soldiers.Archer;
 import com.company.Models.Soldiers.Soldier;
 import com.company.UIs.AttackMapUI;
 import com.company.UIs.MapUI;
 import com.company.UIs.SideBarUI;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Screen;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
@@ -13,6 +19,7 @@ import java.io.ObjectInputStream;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.util.ArrayList;
+import java.util.Map;
 
 public class UDPReceiver extends Thread {
     private boolean running;
@@ -50,7 +57,27 @@ public class UDPReceiver extends Thread {
 //                    archer.setY(1);
 //                    liveStreamingMessage.troops.add(archer);
 //                }
-                MapUI.getController().getGame().setTroops(liveStreamingMessage1.getTroops());
+                ArrayList<Soldier> soldiers = new ArrayList<>();
+                for (Soldier soldier : liveStreamingMessage1.getTroops()) {
+                    if(soldier.isHasPut() && soldier.getX()== -1){
+                        continue;
+                    }
+                    soldiers.add(soldier);
+                }
+                MapUI.getController().getGame().setTroops(soldiers);
+                for (Soldier soldier : MapUI.getController().getGame().getTroops()) {
+
+                    soldier.setImageView(new ImageView());
+                    Rectangle leftHealth=new Rectangle((1.0* Screen.getPrimary().getVisualBounds().getHeight() / 32)*soldier.getHealth()/ Config.getDictionary().get(soldier.getClass().getSimpleName() + "_HEALTH"),1);
+                    leftHealth.setFill(Color.rgb(6,87,51));
+                    soldier.setLeftHealth(leftHealth);
+                    Rectangle allHealth=new Rectangle(5,1);
+                    allHealth.setFill(Color.rgb(159,15,55));
+                    soldier.setAllHealth(allHealth);
+//                    AttackMapUI.putSoldiersImageInMap(soldier.getX(),soldier.getY(),32,AttackMapUI.canvas,AttackMapUI.getSoldiersGif().get(soldier.getClass().getSimpleName()+"MoveUp"),soldier,);
+                }
+
+                MapUI.getController().getGame().getVillage().setResource(new Resource(10,10));
                 MapUI.getController().getGame().setAttackedVillage(MapUI.getController().getGame());
                 MapUI.isIsInDefense(true);
 //                AttackMapUI.makeAttackGameBoard(SideBarUI.primaryStage,MapUI.getController());
